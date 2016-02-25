c47917beba8aa30ddd8c63fa613087646ae2635b
vince scott <c0d3x42@gmail.com>
2016-02-25 21:33:32 +0000

    use common instead

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,13 +14,22 @@
 # limitations under the License.
 #
 
+FORCE_32_BIT := true
+
+-include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+DEVICE_PATH := device/zte/p839v55
+
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8916
-TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
+# Camera
+#BOARD_CAMERA_SENSORS := \
+#    s5k3m2xm \
+#    imx214
+#TARGET_USE_VENDOR_CAMERA_EXT := true
+#USE_DEVICE_SPECIFIC_CAMERA := true
+
 # Platform
-TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
 
 # Architecture
@@ -32,38 +41,50 @@ TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+# CPU
+TARGET_CPU_CORTEX_A53 := true
+
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/zte/msm8916
-TARGET_KERNEL_CONFIG := msm_p839v55_defconfig
-TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_CUSTOM_BOOTIMG_MK := device/zte/p839v55/mkbootimg.mk
+#TARGET_KERNEL_SOURCE := kernel/zte/msm8916
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+TARGET_KERNEL_SOURCE := kernel/zte/vsu6
+TARGET_KERNEL_CONFIG := msm_P839V55_defconfig
+#TARGET_KERNEL_CONFIG := msm8916_defconfig
+#TARGET_KERNEL_SOURCE := kernel/zte/VF-995N
+TARGET_KERNEL_CONFIG := msm_P839V55-PV_defconfig
+TARGET_KERNEL_ARCH := arm
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 loglevel=7
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+#BOARD_RAMDISK_OFFSET     := 0x02000000
+#BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+
 #BOARD_DTBTOOL_ARGS := -2
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/zte/p839v55/dt.img
+##BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/zte/p839v55/dt.img
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 
+
+
+
+
+# Kernel
+##TARGET_KERNEL_CONFIG := msm_P839V55_m_defconfig
+##TARGET_KERNEL_ARCH := arm
+##BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+#BOARD_KERNEL_SEPARATED_DT := true
+##BOARD_RAMDISK_OFFSET     := 0x01000000
+##BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+#BOARD_DTBTOOL_ARGS := -2
+#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/zte/p839v55/dt.img
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := P839v55,p839v55
 
 # ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-smd"
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_DEEP_BUFFER_RINGTONE := true
-AUDIO_FEATURE_ENABLED_FM := true
-AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/p839v55/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
+#BOARD_ANT_WIRELESS_DEVICE := "qualcomm-smd"
 
 # Charger
 BOARD_CHARGER_SHOW_PERCENTAGE := true
@@ -86,31 +107,9 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 318767104
 BOARD_PERSISTIMAGE_PARTITION_SIZE  := 33554432
 BOARD_FLASH_BLOCK_SIZE             := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
-# FM
-TARGET_QCOM_NO_FM_FIRMWARE := true
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
 # Graphics
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-USE_OPENGL_RENDERER := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# Shader cache config options
-# Maximum size of the GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-# Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
@@ -118,47 +117,22 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Memory
 MALLOC_IMPL := dlmalloc
 
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-
 # Recovery
 TARGET_RECOVERY_FSTAB := device/zte/p839v55/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 COMMON_GLOBAL_CFLAGS += -DRECOVERY_FONT='"roboto_15x24.h"'
 
-# RIL
-TARGET_RIL_VARIANT := caf
-PROTOBUF_SUPPORTED := true
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/zte/p839v55/sepolicy
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 32
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
 BOARD_WLAN_DEVICE := qcwcn
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/pronto/pronto_wlan.ko"
 WIFI_DRIVER_MODULE_NAME := "wlan"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/zte/p839v55/BoardConfigVendor.mk
